741841fcc4ad64efc2bf4943dd874024557d5ee8
Atul Yadav <atulyadavmca@gmail.com>
2025-02-27 11:57:04 +0530

    new changes

--- src/main/java/com/cotodel/hrms/web/controller/StaticPageController.java
+++ src/main/java/com/cotodel/hrms/web/controller/StaticPageController.java
@@ -1117,7 +1117,7 @@ public class StaticPageController extends CotoDelBaseController{
 		if(token!=null) {
 			UserDetailsEntity obj = JwtTokenValidator.parseToken(token);
 			if(obj!=null) {
-				if(obj.getUser_role()==9 || obj.getUser_role()==3) {
+				if(obj.getUser_role()==9) {
 				model.addAttribute("name",obj.getName());
 				model.addAttribute("org",obj.getOrgName());
 				model.addAttribute("mobile",obj.getMobile());
@@ -1306,7 +1306,7 @@ public class StaticPageController extends CotoDelBaseController{
 			if(token!=null) {
 				UserDetailsEntity obj = JwtTokenValidator.parseToken(token);
 				if(obj!=null) {
-					if(obj.getUser_role()==9 || obj.getUser_role()==1 || obj.getUser_role()==3) {
+					if(obj.getUser_role()==2) {
 					model.addAttribute("name",obj.getName());
 					model.addAttribute("org",obj.getOrgName());
 					model.addAttribute("mobile",obj.getMobile());
