e1e140b71076d18fb0887bb88a07dedbfdcf7b99
Anish Mokashi <anish.mokashi@cotodel.com>
2025-05-16 10:05:55 +0530

    vehicle management

--- src/main/java/com/cotodel/hrms/web/controller/StaticPageController.java
+++ src/main/java/com/cotodel/hrms/web/controller/StaticPageController.java
@@ -1811,4 +1811,26 @@ public class StaticPageController extends CotoDelBaseController{
 	}
 	return new ModelAndView("index", "command", "");
 }
+    @GetMapping(value="/editvehicle")
+   	public ModelAndView editvehicle(Model model) {
+   		String token = (String) session.getAttribute("hrms");
+   		Integer id  = (Integer) session.getAttribute("id");
+   		if(token!=null) {
+   			UserDetailsEntity obj = JwtTokenValidator.parseToken(token);
+   			if(obj!=null) {
+   				if(obj.getUser_role()==9) {
+   				model.addAttribute("name",obj.getName());
+   				model.addAttribute("org",obj.getOrgName());
+   				model.addAttribute("mobile",obj.getMobile());
+   				model.addAttribute("email",obj.getEmail());
+   				model.addAttribute("employerId",id);
+    
+   				return new ModelAndView("edit-vehicle", "command", "");
+   			}
+   			 return new ModelAndView("error", "command", "");
+   		}
+   		return new ModelAndView("index", "command", "");
+   	}
+   	return new ModelAndView("index", "command", "");
+   }
 }
