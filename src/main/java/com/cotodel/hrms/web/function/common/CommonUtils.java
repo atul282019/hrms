53398aca9be270de53e78d90c0ce2cdd9607ff5b
Anish Mokashi <anish.mokashi@cotodel.com>
2025-05-21 15:47:07 +0530

    CommonUtils

--- src/main/java/com/cotodel/hrms/web/function/common/CommonUtils.java
+++ src/main/java/com/cotodel/hrms/web/function/common/CommonUtils.java
@@ -150,6 +150,7 @@ public interface CommonUtils {
 		 
 	public static String getVehicleManagementList="/empService/Api/get/getVehicleManagement";
 	public static String addVehicleDetails="/empService/Api/add/VehicleManagement";
+	
 	//public static String savemanagerMasterDetailS="/empService/Api/add/managerMasterDetails";
 	//public static String getmanagerMasterDetailSwithId="/empService/Api/get/managerMasterList";
 	//public static String savejobTitlemasterDetailS="/empService/Api/add/jobTitleMasterDetails";
