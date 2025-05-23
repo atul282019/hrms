451f48e6caf5e67523c27e422aa138ec4c4559a6
Anish Mokashi <anish.mokashi@cotodel.com>
2025-05-23 11:00:42 +0530

    vehicle-management changes

--- src/main/java/com/cotodel/hrms/web/controller/StaticPageController.java
+++ src/main/java/com/cotodel/hrms/web/controller/StaticPageController.java
@@ -1744,28 +1744,28 @@ public class StaticPageController extends CotoDelBaseController{
 	return new ModelAndView("index", "command", "");
 }
     
-//    @GetMapping(value="/editvehicle")
-//	public ModelAndView vehiclemanagement(Model model) {
-//		String token = (String) session.getAttribute("hrms");
-//		Integer id  = (Integer) session.getAttribute("id");
-//		if(token!=null) {
-//			UserDetailsEntity obj = JwtTokenValidator.parseToken(token);
-//			if(obj!=null) {
-//				if(obj.getUser_role()==9) {
-//				model.addAttribute("name",obj.getName());
-//				model.addAttribute("org",obj.getOrgName());
-//				model.addAttribute("mobile",obj.getMobile());
-//				model.addAttribute("email",obj.getEmail());
-//				model.addAttribute("employerId",id);
-// 
-//				return new ModelAndView("vehicle-management", "command", "");
-//			}
-//			 return new ModelAndView("error", "command", "");
-//		}
-//		return new ModelAndView("index", "command", "");
-//	}
-//	return new ModelAndView("index", "command", "");
-//}
+    @GetMapping(value="/editvehicle")
+	public ModelAndView vehiclemanagement(Model model) {
+		String token = (String) session.getAttribute("hrms");
+		Integer id  = (Integer) session.getAttribute("id");
+		if(token!=null) {
+			UserDetailsEntity obj = JwtTokenValidator.parseToken(token);
+			if(obj!=null) {
+				if(obj.getUser_role()==9) {
+				model.addAttribute("name",obj.getName());
+				model.addAttribute("org",obj.getOrgName());
+				model.addAttribute("mobile",obj.getMobile());
+				model.addAttribute("email",obj.getEmail());
+				model.addAttribute("employerId",id);
+ 
+				return new ModelAndView("vehicle-management", "command", "");
+			}
+			 return new ModelAndView("error", "command", "");
+		}
+		return new ModelAndView("index", "command", "");
+	}
+	return new ModelAndView("index", "command", "");
+}
     @GetMapping(value="/bulkvehicleupload")
 	public ModelAndView bulkvehicleupload(Model model) {
 		String token = (String) session.getAttribute("hrms");
