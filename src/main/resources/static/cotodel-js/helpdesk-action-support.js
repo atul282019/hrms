f6d836587031c17d2b6718deb74782dd9f41bae6
Atul Yadav <atulyadavmca@gmail.com>
2025-07-16 10:13:48 +0530

    new file

---
+++ src/main/resources/static/cotodel-js/helpdesk-action-support.js
@@ -0,0 +1,212 @@
+function convertImageToBase64() {
+           const fileInput = document.getElementById('fileInput');
+           const output = document.getElementById('base64Output');
+		   const outputFileName = document.getElementById('fileName');
+		  
+		  
+           if (!fileInput.files || fileInput.files.length === 0) {
+			 document.getElementById("fileInputError").innerHTML="Please select file";
+			 return;
+           }
+           var filePath = fileInput.value;
+               var allowedExtensions =  /(\.jpg|\.jpeg|\.png|)$/i;
+			  // var allowedExtensions =  /(\.xlsx)$/i;
+               if (!allowedExtensions.exec(filePath)) {
+				  document.getElementById("fileInputError").innerHTML="Invalid file type";
+                   fileInput.value = '';
+                   return false;
+               } 
+			   else{
+				document.getElementById("fileInputError").innerHTML="";
+			   }
+			  		
+           const file = fileInput.files[0];	   
+           const reader = new FileReader();
+           reader.onload = function(event) {
+               const base64String = event.target.result.split(',')[1]; 
+               output.value = base64String; 
+			   console.log("FileName:", fileInput.files[0].name);
+			   outputFileName.value =fileInput.files[0].name;
+			
+		//	   document.querySelector('.form-group.choose-file .form-control').value = fileInput.files[0].name;
+           };
+           reader.readAsDataURL(file);
+		  // document.getElementById("bulksubmit").disabled=false;
+    }
+
+
+	async function replyTicket(){
+		var ticketId = document.getElementById("ticketId").value;
+		var fileInput = document.getElementById("fileInput").value;
+		var base64file = document.getElementById("base64Output").value;
+		var createdby =  document.getElementById("employerMobile").value;
+		var orgId = document.getElementById("employerId").value;
+		var issueDesc = document.getElementById("replyText").value;
+
+		const clientKey = "client-secret-key"; // Extra security measure
+		const secretKey = "0123456789012345"; // SAME KEY AS BACKEND
+
+	    // Concatenate data (must match backend)
+		const dataString = orgId+issueDesc+createdby+base64file+clientKey+secretKey;
+
+		const encoder = new TextEncoder();
+		const data = encoder.encode(dataString);
+		const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+		const hashArray = Array.from(new Uint8Array(hashBuffer));
+		const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+		
+	
+		if(issueDesc =="" || issueDesc == null){
+				 document.getElementById("fileInputError").innerHTML="Please Enter Error Description";
+				 return false;
+			}
+			else{
+				document.getElementById("fileInputError").innerHTML="";
+			}
+		if(fileInput =="" || fileInput == null){
+			 document.getElementById("fileInputError").innerHTML="Please Select File";
+			 return false;
+		}
+		else{
+			document.getElementById("fileInputError").innerHTML="";
+		}
+		document.getElementById("signinLoader").style.display="flex";
+		$.ajax({
+			type: "POST",
+		     url:"/replyTicket",
+			 dataType: 'json',   
+		      data: {
+				        "id":ticketId,
+						"orgId":orgId,
+						"issueDesc":issueDesc,
+						"ticketImg":base64file,
+						"createdby":createdby,
+						"clientKey":clientKey,
+					    "hash":hashHex
+				 },  		 
+		        success:function(data){
+				document.getElementById("signinLoader").style.display="none";
+		        var data1 = data.data;
+			    var data2 = jQuery.parseJSON(data1);
+			    console.log("data2",data2);
+				if(data2.status==true){
+					document.getElementById("replyText").value=""; 
+					document.getElementById("fileName").innerHTML=""; 
+					
+				}else if(data.status==false){
+					$('#AddVehicleModal').modal('show');
+				}
+		     },
+		     error: function(e){
+		         alert('Error: ' + e);
+		     }
+		});	
+		
+	}
+
+async function getSupportTicketActionList() {
+  var orgId = document.getElementById("employerId").value;
+  document.getElementById("signinLoader").style.display="flex";
+  $.ajax({
+    type: "GET",
+    url: "/getTicketListForAction",
+    data: {
+      orgId: orgId
+    },
+    beforeSend: function (xhr) {},
+    success: function (data) {
+	  document.getElementById("signinLoader").style.display="none";
+      const data1 = jQuery.parseJSON(data);
+   		var data2 = data1.data;
+      $('#TicketSupportActionTable').DataTable({
+        destroy: true,
+        responsive: true,
+        searching: false,
+        bInfo: false,
+        paging: false,
+        lengthChange: true,
+        autoWidth: false,
+        pagingType: "full_numbers",
+        pageLength: 50,
+        buttons: ["copy", "csv", "excel", "pdf", "print", "colvis"],
+        language: { emptyTable: "No Tickets Found" },
+        aaData: data2,
+		aoColumns: [
+		  { mData: "name" },
+		  { mData: "organizationName" },
+		  { mData: "mobile" },
+		  { mData: "ticketNo" },
+		  { mData: "issueType" },
+		 
+		  { mData: "subject" },
+		  {
+		    mData: "status",
+		    render: function (data) {
+		      return data === 0 ? "Submitted" : "Closed";
+		    }
+		  },
+		  { mData: "creationDate"},
+		  {
+		    mData: "status",
+		    render: function (data, type, row) {
+		      const ticketId = row.id; // or use row.id if that's your identifier
+		      return `<a href="/helpdeskadmin?ticketId=${ticketId}"> 
+		                <img src="img/edit-pencil-blue.png" alt="Edit Pencil" 
+		                     style="height:24px; width:24px; margin-right:4px;"> 
+		              </a>`;
+		    }
+		  }
+		]
+      });
+    },
+    error: function (e) {
+      alert('Failed to fetch JSON data' + e);
+    }
+  });
+}
+
+
+async function getTicketDetailById() {
+  var orgId = document.getElementById("employerId").value;
+  var ticketId = document.getElementById("ticketId").value;
+  document.getElementById("signinLoader").style.display="flex";
+  $.ajax({
+    type: "GET",
+    url: "/getTicketDetailByTicketId",
+    data: {
+      orgId: orgId,
+	  "id":ticketId
+    },
+    beforeSend: function (xhr) {},
+    success: function (data) {
+	  document.getElementById("signinLoader").style.display="none";
+      const data1 = jQuery.parseJSON(data);
+   	  var data2 = data1.data;
+	  const rawDate = new Date(data2.creationDate);
+
+	  // Format: 'Wed, 09/07'
+	  const options = { weekday: 'short' };
+	  const weekday = rawDate.toLocaleDateString('en-US', options);
+	  const day = String(rawDate.getDate()).padStart(2, '0');
+	  const month = String(rawDate.getMonth() + 1).padStart(2, '0');
+
+	  document.getElementById("tickeNo").innerHTML=data2.ticketNo;
+	  document.getElementById("senderName").innerHTML=data2.name;
+	  document.getElementById("companyName").innerHTML=data2.organizationName;
+	  document.getElementById("dateTime").innerHTML=data2.creationDate;
+	  document.getElementById("status").innerHTML=data2.status;
+	  document.getElementById("subject").innerHTML=data2.subject;
+	  document.getElementById("submittedBy").innerHTML=data2.name;
+	  document.getElementById("submittedDate").innerHTML=`${weekday}, ${day}/${month}`;
+	  document.getElementById("submittedDate1").innerHTML=`${weekday}, ${day}/${month}`;
+	  
+	  document.getElementById("ticketDetail").innerHTML=data2.subject;
+	  
+	 document.getElementById("imgTicket").src = "data:image/png;base64," + data2.ticketImg;
+	  
+    },
+    error: function (e) {
+      alert('Failed to fetch JSON data' + e);
+    }
+  });
+}
