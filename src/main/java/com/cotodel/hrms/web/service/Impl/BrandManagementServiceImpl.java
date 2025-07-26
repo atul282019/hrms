2ff0b7d1545f8f9dfc9ef3d50f5b5b3c25a10714
Atul Yadav <atulyadavmca@gmail.com>
2025-07-26 15:43:30 +0530

    bulk outlet template

--- src/main/java/com/cotodel/hrms/web/service/Impl/BrandManagementServiceImpl.java
+++ src/main/java/com/cotodel/hrms/web/service/Impl/BrandManagementServiceImpl.java
@@ -64,4 +64,10 @@ public class BrandManagementServiceImpl implements BrandManagementService{
 			    );
 	}
 
+	@Override
+	public String saveBulkOutletDetail(String token, EncriptResponse jsonObject) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
